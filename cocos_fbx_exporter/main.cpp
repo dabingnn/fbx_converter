7f610898d944d5eaf2a64f570baad8895a640c87
Huabing.Xu <dabingnn@gmail.com>
2016-10-26 23:10:12 +0800

    Initial Commit

---
+++ cocos_fbx_exporter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cocos_fbx_exporter
+//
+//  Created by Huabing.Xu on 16/10/26.
+//  Copyright © 2016年 Chukong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
